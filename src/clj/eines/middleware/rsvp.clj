29124b01908be043899917abd9c2120070dcdf84
Jarppe <jarppe@metosin.fi>
2017-01-18 17:59:29 +0200

    Allow configuring what keys are sen't in responses, by default use only :body.

--- src/clj/eines/middleware/rsvp.clj
+++ src/clj/eines/middleware/rsvp.clj
@@ -20,34 +20,39 @@
 (defn- rsvp-response-id [message]
   (-> message :headers :eines/rsvp-response-id))
 
-(defn rsvp-middleware []
-  (let [next-request-id (let [id (atom 0)]
-                          (fn [] (swap! id inc)))
-        rsvp-requests (agent {} :error-handler (fn [_ e]
-                                                 (log/error e "unexpected error in rsvp agent")))]
-    {:in (fn [handler]
-           (fn [message]
-             (cond
-               (rsvp-request-id message) (let [send! (-> message :send!)
-                                               response (handler message)]
-                                           (-> response
-                                               (assoc-in [:headers :eines/rsvp-response-id] (rsvp-request-id message))
-                                               (assoc :type :eines.type/response)
-                                               (send!))
-                                           response)
-               (rsvp-response-id message) (do (send rsvp-requests
-                                                    rsvp-completed
-                                                    (rsvp-response-id message)
-                                                    message)
-                                              nil)
-               :else (handler message))))
-     :out (fn [handler]
+(defn rsvp-middleware
+  ([] (rsvp-middleware [:body]))
+  ([response-keys]
+   {:pre [(->> response-keys seq)
+          (->> response-keys (every? keyword?))]}
+   (let [next-request-id (let [id (atom 0)]
+                           (fn [] (swap! id inc)))
+         rsvp-requests (agent {} :error-handler (fn [_ e]
+                                                  (log/error e "unexpected error in rsvp agent")))]
+     {:in (fn [handler]
             (fn [message]
-              (handler (if-let [rsvp-ctx (-> message :headers :eines/rsvp)]
-                         (let [request-id (next-request-id)
-                               timeout (start-timeout rsvp-requests request-id rsvp-ctx)]
-                           (send rsvp-requests assoc request-id (assoc rsvp-ctx :future timeout))
-                           (-> message
-                               (update :headers dissoc :eines/rsvp)
-                               (update :headers assoc :eines/rsvp-request-id request-id)))
-                         message))))}))
+              (cond
+                (rsvp-request-id message) (let [send! (-> message :send!)
+                                                response (handler message)]
+                                            (-> response
+                                                (select-keys response-keys)
+                                                (assoc-in [:headers :eines/rsvp-response-id] (rsvp-request-id message))
+                                                (assoc :type :eines.type/response)
+                                                (send!))
+                                            response)
+                (rsvp-response-id message) (do (send rsvp-requests
+                                                     rsvp-completed
+                                                     (rsvp-response-id message)
+                                                     message)
+                                               nil)
+                :else (handler message))))
+      :out (fn [handler]
+             (fn [message]
+               (handler (if-let [rsvp-ctx (-> message :headers :eines/rsvp)]
+                          (let [request-id (next-request-id)
+                                timeout (start-timeout rsvp-requests request-id rsvp-ctx)]
+                            (send rsvp-requests assoc request-id (assoc rsvp-ctx :future timeout))
+                            (-> message
+                                (update :headers dissoc :eines/rsvp)
+                                (update :headers assoc :eines/rsvp-request-id request-id)))
+                          message))))})))
