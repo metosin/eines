db437320e3fa864f60b9effeb4a74548852ccfe3
Jarppe <jarppe@metosin.fi>
2017-02-12 16:56:48 +0200

    0.0.7-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject metosin/eines "0.0.6"
+(defproject metosin/eines "0.0.7-SNAPSHOT"
   :description "Simple clj/cljs library for WebSocket communication"
   :url "https://github.com/metosin/eines"
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
