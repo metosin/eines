154c3735f78f230b2605d00277fb921b0fbc291f
Juho Teperi <juho.teperi@iki.fi>
2017-10-30 16:22:10 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [; Client
-                 [com.cognitect/transit-cljs "0.8.239"]
+                 [com.cognitect/transit-cljs "0.8.243"]
                  ; Server
-                 [org.clojure/tools.logging "0.3.1"]
-                 [com.cognitect/transit-clj "0.8.297"]
-                 [cheshire "5.6.3"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [com.cognitect/transit-clj "0.8.300"]
+                 [cheshire "5.8.0"]
                  ; Optionals
-                 [org.immutant/web "2.1.6" :scope "provided"]]
+                 [org.immutant/web "2.1.9" :scope "provided"]]
 
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [org.clojure/clojurescript "1.9.293" :exclusions [com.google.code.findbugs/jsr305]]
-                                  [org.immutant/web "2.1.6"]]}}
+                                  [org.clojure/clojurescript "1.9.946" :exclusions [com.google.code.findbugs/jsr305]]
+                                  [org.immutant/web "2.1.9"]]}}
 
   :global-vars {*warn-on-reflection* true}
 
