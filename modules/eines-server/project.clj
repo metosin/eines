d2dc5c39bd3cf06144c4373876437871697c664b
Miikka Koskinen <miikka@metosin.fi>
2018-05-18 18:10:45 +0300

    Split Eines into modules

---
+++ modules/eines-server/project.clj
@@ -0,0 +1,11 @@
+(defproject metosin/eines-server "0.0.10-SNAPSHOT"
+  :description "Simple clj/cljs library for WebSocket communication"
+  :url "https://github.com/metosin/eines"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-parent "0.3.2"]]
+  :parent-project {:path "../../project.clj"
+                   :inherit [:deploy-repositories :managed-dependencies]}
+  :dependencies [[org.clojure/tools.logging]
+                 [com.cognitect/transit-clj]
+                 [cheshire]])
--- project.clj
+++ project.clj
@@ -1,21 +1,26 @@
-(defproject metosin/eines "0.0.10-SNAPSHOT"
+(defproject metosin/eines-parent "0.0.10-SNAPSHOT"
   :description "Simple clj/cljs library for WebSocket communication"
   :url "https://github.com/metosin/eines"
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [; Client
-                 [com.cognitect/transit-cljs "0.8.243"]
-                 ; Server
-                 [org.clojure/tools.logging "0.4.0"]
-                 [com.cognitect/transit-clj "0.8.300"]
-                 [cheshire "5.8.0"]
-                 ; Optionals
-                 [org.immutant/web "2.1.10" :scope "provided"]]
+  :managed-dependencies [[metosin/eines "0.0.10-SNAPSHOT"]
+                         [metosin/eines-client "0.0.10-SNAPSHOT"]
+                         [metosin/eines-immutant "0.0.10-SNAPSHOT"]
+                         [metosin/eines-server "0.0.10-SNAPSHOT"]
 
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
-                                  [org.clojure/clojurescript "1.9.946" :exclusions [com.google.code.findbugs/jsr305]]
-                                  [org.immutant/web "2.1.10"]]
+                         [com.cognitect/transit-cljs "0.8.243"]
+                         [org.clojure/tools.logging "0.4.0"]
+                         [com.cognitect/transit-clj "0.8.300"]
+                         [cheshire "5.8.0"]
+                         [org.immutant/web "2.1.10"]]
+
+  :profiles {:dev {:source-paths ["modules/eines/src"
+                                  "modules/eines-client/src"
+                                  "modules/eines-immutant/src"
+                                  "modules/eines-server/src"]
+                   :dependencies [[metosin/eines]
+                                  [org.clojure/clojure "1.9.0"]
+                                  [org.clojure/clojurescript "1.9.946" :exclusions [com.google.code.findbugs/jsr305]]]
                    :global-vars {*warn-on-reflection* true}}}
 
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
   :test-paths ["test/clj" "test/cljc"])
